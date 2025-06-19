265e5d73395aa7e510cd59923d746b0bfd3a5dac
Filollan <mllanten@unimayor.edu.co>
2025-06-19 18:10:39 -0500

    agg @input

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesModule } from "../../heroes/heroes.module";
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -10,5 +11,20 @@ import { HeroesModule } from "../../heroes/heroes.module";
 })
 
 export class MainPageComponent {
-
+ public characters : Character[] = [
+  {
+    name: 'Goku',
+    power: 15000,
+  }, {
+    name: 'Vegeta',
+    power: 14000,
+  },
+  {
+    name: 'Gohan',
+    power: 12000,
+  }, {
+    name: 'Piccolo',
+    power: 8000,
+  }
+ ]
 }
